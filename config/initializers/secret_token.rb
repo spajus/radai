dfa8986ae8a6392eef905719f77d5b58b0a1aa02
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-09-24 15:29:31 +0300

    Intro page design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radai::Application.config.secret_token = '4e279d7d1322dcb40c9571608342bced446bcc5b223f090bc592b12316521426cee025fd34800a6efb0f7595ae267c1bdeb8e28716e1b2edcadb781e425824b2'
