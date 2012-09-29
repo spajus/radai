e626de4eaf2fc673f0024638e9d3bd8c74dd0e8e
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-09-29 12:36:14 +0300

    Default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Radai::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'statybos.radai.lt' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "localhost",
