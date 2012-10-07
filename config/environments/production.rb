7bc524a5839cfeb02eaec9ff83d6e8b3505b25ad
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-10-07 11:05:30 +0300

    locale fallbacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ Radai::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  # rails will fallback to en, no matter what is set as config.i18n.default_locale
+  config.i18n.fallbacks = [:en]
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
