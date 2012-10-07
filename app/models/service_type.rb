11cda6ff63c65cc9ad5070ffa52735aa56b82d6b
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-10-07 12:16:05 +0300

    Show only provided services in search

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -9,4 +9,11 @@ class ServiceType < ActiveRecord::Base
 
   has_many :specialist_services
   has_many :specialists, through: :specialist_services
+
+  def self.find_provided
+    SpecialistService.joins(:service_type)
+      .order("service_types.title")
+      .all(select: "distinct service_types.id, service_types.title")
+      .collect { |s| { id: s.id, title: s.title } }
+  end
 end
